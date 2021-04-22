ecd4d41815db16de0b9f383de7bc8bd2313ad52a
Kunal Arora <10kunalji@gmail.com>
2021-04-22 23:50:32 +0530

    Will change CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      newItem: "",
+      list: []
+    }
+  };
+
+  addItem(todoValue){
+    if(todoValue!== ""){
+      const newItem={
+        id: Date.now(),
+        value: todoValue,
+        isDone: false,
+      };
+      const list =[...this.state.list];
+      list.push(newItem);
+
+      this.setState({
+        list,
+        newItem: ""
+      })
+
+    }
+
+  }
+  deleteItem(id){
+    const list =[...this.state.list];
+    const updatedlist =list.filter(item => item.id !==id);
+    this.setState({list: updatedlist})
+  }
+
+  updateImput(input){
+    this.setState({newItem: input});
+  }
+
+  render(){
+    return(
+      <div className="todo">
+        <h1 className="appName">Todo List</h1>
+        <div className="container">
+          Add an item..
+          <br />
+          <input type="text" className="input-text" placeholder="Write a Todo" required value={this.state.newItem}
+          onChange={e => this.updateImput(e.target.value)} />
+          <button className="addBtn" onClick={()=> this.addItem(this.state.newItem)} disabled={!this.state.newItem.length}>
+          Add TODO</button>
+          <ul>
+          {this.state.list.map((item)=>{
+            return(
+              <div className="todoDis">
+              <li key={item.id}>
+              
+              {item.value}
+              <button className="btn" onClick={()=>this.deleteItem(item.id)}>Delete</button>
+            </li>
+              </div>
+             
+            );
+          })}
+          <div className="todoDis"><li>
+              Record Vidos
+              <button className="btn">Delete</button>
+            </li></div>
+            
+          </ul>
+        </div>
+
+
+      </div>
+    ); 
+  };
 }
 
 export default App;
